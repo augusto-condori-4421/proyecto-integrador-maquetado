c10c1ae06e9361f28512ebe5e65bcd9fc0ad98be
Augusto-Condori <augustocondori1@gmail.com>
2025-03-06 22:12:50 -0300

    terminado footer, agregado de animaciones de entrada a header y footer, estilos y animaciones hover a links y botones, fijado de header a cierto nivel de scroll, falta el menu hamburguesa para mobile

--- scripts.js
+++ scripts.js
@@ -14,4 +14,39 @@ document.addEventListener('DOMContentLoaded', () => { // espera a que el dom car
 
     // llama a la funcion observer
     elements.forEach(element => observer.observe(element));
+
+    // Animaciones para elementos que suben y bajan, funcion upDownElements replica de la funcion observer
+    const upDownElements = document.querySelectorAll('.up, .down');
+    
+    const upDownObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const element = entry.target;
+                const animation = element.classList.contains('up') ? 'animate__fadeInUp' : 'animate__fadeInDown';
+                element.classList.add('animate__animated', animation);
+                upDownObserver.unobserve(element);
+            }
+        });
+    }, { threshold: 0.2 });
+
+    upDownElements.forEach(element => upDownObserver.observe(element));
+});
+
+// Manejo del encabezado, barra de navegacion
+// funcion: que el encabezado se fije al quedar fuera de vision por escrollear
+const header = document.querySelector("header");
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    let currentScroll = window.scrollY;
+
+    if (currentScroll > lastScroll && currentScroll > 400) {
+        // Si scrolleaste hacia abajo y pasaste los 100px, se fija con animación
+        header.classList.add("fixed");
+    } else if (currentScroll <= 50) {
+        // Si vuelves arriba, se quita la clase fixed
+        header.classList.remove("fixed");
+    }
+
+    lastScroll = currentScroll;
 });
